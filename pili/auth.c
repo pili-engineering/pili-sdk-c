40ca7a9a225ef97d9b531a85f330f59b0cd7f22c
Jemy <jemygraw@gmail.com>
2017-03-18 19:58:14 +0800

    refractor the code of auth

--- pili/auth.c
+++ pili/auth.c
@@ -20,14 +20,14 @@ const char *pili_hmac_sha1(const char *access_key, const char *secret_key, const
     HMAC_Final(&ctx, digest, &digest_len);
     HMAC_cleanup(&ctx);
 
-    size_t dst_encoded_len = urlsafe_b64_encode(digest, digest_len, NULL, 0);
-    char *dst_encoded_str = (char *) malloc(dst_encoded_len + 1);
-    memset(dst_encoded_str, 0, dst_encoded_len + 1);
+    size_t dst_encoded_len = urlsafe_b64_encode(digest, digest_len, NULL, 0) + 1;
+    char *dst_encoded_str = (char *) malloc(dst_encoded_len);
+    memset(dst_encoded_str, 0, dst_encoded_len);
     urlsafe_b64_encode(digest, digest_len, dst_encoded_str, dst_encoded_len);
 
-    size_t final_token_len = strlen(access_key) + strlen(dst_encoded_str) + 1;
-    char *final_token = (char *) malloc(sizeof(char) * (final_token_len + 1));
-    memset(final_token, 0, final_token_len + 1);
+    size_t final_token_len = strlen(access_key) + strlen(dst_encoded_str) + 2;
+    char *final_token = (char *) malloc(sizeof(char) * (final_token_len));
+    memset(final_token, 0, final_token_len);
     sprintf(final_token, "%s:%s", access_key, dst_encoded_str);
     free((void *) dst_encoded_str);
     return final_token;
@@ -35,58 +35,19 @@ const char *pili_hmac_sha1(const char *access_key, const char *secret_key, const
 
 const char *pili_sign_request(const char *access_key, const char *secret_key, const char *host, const char *method,
                               const char *path, const char *content_type, const char *body, const char *query) {
-    char data_to_sign[1024];
-    memset(data_to_sign, NULL, 1024);
-    char *data_offset = data_to_sign;
-
-    //method
-    strcpy(data_offset, method);
-    data_offset += strlen(method);
-
-    //space
-    *data_offset = ' ';
-    data_offset += 1;
-
-    //path
-    strcpy(data_offset, path);
-    data_offset += strlen(path);
-
-    //query
-    if (query) {
-        *data_offset = '?';
-        data_offset += 1;
-        strcpy(data_offset, query);
-        data_offset += strlen(query);
-    }
-
-
-    //host
-    strcpy(data_offset, "\nHost: ");
-    data_offset += 7;
-    strcpy(data_offset, host);
-    data_offset += strlen(host);
-
-    //content type
-    if (content_type) {
-        strcpy(data_offset, "\nContent-Type: ");
-        data_offset += 15;
-        strcpy(data_offset, content_type);
-        data_offset += strlen(content_type);
-    }
-
-    strcpy(data_offset, "\n\n");
-    data_offset += 2;
-
-    if (body) {
-        strcpy(data_offset, body);
-    }
-
+    char *data_to_sign_fmt = "%s %s%s%s\nHost: %s\nContent-Type: %s\n\n%s";
+    size_t data_to_sign_len = snprintf(NULL, 0, data_to_sign_fmt, method, path,
+                                       query ? "?" : "", query ? query : "", host, content_type, body ? body : "") + 1;
+    char *data_to_sign = (char *) malloc(sizeof(char) * data_to_sign_len);
+    sprintf(data_to_sign, data_to_sign_fmt, method, path,
+            query ? "?" : "", query ? query : "", host, content_type, body ? body : "");
+    //printf("%s\n", data_to_sign);
     const char *sign = pili_hmac_sha1(access_key, secret_key, data_to_sign);
     size_t dst_token_len = strlen(sign) + 7;
     char *final_token = (char *) malloc(dst_token_len);
-    memset(final_token, 0, dst_token_len);
 
     sprintf(final_token, "Qiniu %s", sign);
+    free((void *) data_to_sign);
     free((void *) sign);
     return final_token;
 }
